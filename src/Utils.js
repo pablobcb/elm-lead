6082fb7612db18528cc381262f6e43744808e3ba
Alessandro Delgado <adelgado1313@gmail.com>
2016-05-29 20:56:47 +0200

    Add logging function for MIDI messages to Utils

--- src/Utils.js
+++ src/Utils.js
@@ -4,4 +4,13 @@ export default {
 		return 440 * Math.pow(2, (note - 69) / 12)
 	}
 
+	// function logger(container, label, data) {
+	// 	const messages = label + ' [channel: ' + (data[0] & 0xf) +
+	// 		', cmd: ' +
+	// 		(data[0] >> 4) + ', type: ' + (data[0] & 0xf0) + ' , note: ' +
+	// 		data[1] + ' , velocity: ' + data[2] + ']'
+
+	// 	console.log(messages)
+	// }
+
 }
