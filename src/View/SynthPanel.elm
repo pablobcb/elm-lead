043644527086bb4ce51a42057e4580e02faef39e
Pablo Botelho <pablo.bcb@gmail.com>
2016-06-02 03:11:58 -0300

    modularized view components

--- src/View/SynthPanel.elm
+++ src/View/SynthPanel.elm
@@ -11,88 +11,128 @@ import Msg exposing (..)
 
 synthPanel : Html Msg
 synthPanel = 
-    div
-        [ class "synth-panel"]
-        [ masterVolume
-        , oscillatorsBalance
-        , oscillator1Detune
-        , oscillator2Detune
-        ]
+  div
+    [ class "synth-panel" ]
+    [ panelLeftSection
+    , panelMiddleSection
+    , panelRightSection
+    ]
+
+panelLeftSection : Html Msg
+panelLeftSection =
+  div
+    [ class "synth-panel panel-left-section" ]
+    [ masterVolume ]
+
+
+panelMiddleSection : Html Msg
+panelMiddleSection =
+  div
+    [ class "synth-panel panel-middle-section" ]
+    [ oscillators ]
+
+
+panelRightSection : Html Msg
+panelRightSection =
+  div
+    [ class "synth-panel panel-right-section" ]
+    [
+      div
+      []
+      [ span 
+          [] 
+          [ "Breno" |> text ]
+      , input 
+          [ Html.Attributes.type' "range" 
+          , Html.Attributes.min "0"
+          , Html.Attributes.max "100"
+          , Html.Attributes.value "0"
+          , Html.Attributes.step "1"
+          , Html.Events.onInput (\_->NoOp)
+          ]
+         []
+      ]
+    ]
+
+
+oscillators : Html Msg
+oscillators =
+  div
+   [ class "oscillators" ]
+   [ oscillatorsBalance
+   , oscillator1Detune
+   , oscillator2Detune
+   ]
+
 
 masterVolume : Html Msg
 masterVolume = 
-    div
-        []
-        [ span 
-            [] 
-            [ "master level" |> text ]
-        , input 
-            [ Html.Attributes.type' "range" 
-            , Html.Attributes.min "0"
-            , Html.Attributes.max "100"
-            , Html.Attributes.value "70"
-            , Html.Attributes.step "1"
-            , Html.Events.onInput MasterVolumeChange
-            ]
-           []
-
+  div
+    [ class "master-volume"]
+    [ span 
+        [] 
+        [ "master level" |> text ]
+    , input 
+        [ Html.Attributes.type' "range" 
+        , Html.Attributes.min "0"
+        , Html.Attributes.max "100"
+        , Html.Attributes.value "70"
+        , Html.Attributes.step "1"
+        , Html.Events.onInput MasterVolumeChange
         ]
+        []
+    ]
 
 oscillator1Detune : Html Msg
 oscillator1Detune =
-    div
-        []
-        [ span 
-            [] 
-            [ "Oscillator 1 Detune" |> text ]
-        , input 
-            [ Html.Attributes.type' "range" 
-            , Html.Attributes.min "0"
-            , Html.Attributes.max "100"
-            , Html.Attributes.value "0"
-            , Html.Attributes.step "1"
-            , Html.Events.onInput Oscillator1DetuneChange
-            ]
-           []
-
-        ]
+  div
+      []
+      [ span 
+          [] 
+          [ "Oscillator 1 Detune" |> text ]
+      , input 
+          [ Html.Attributes.type' "range" 
+          , Html.Attributes.min "0"
+          , Html.Attributes.max "100"
+          , Html.Attributes.value "0"
+          , Html.Attributes.step "1"
+          , Html.Events.onInput Oscillator1DetuneChange
+          ]
+         []
+      ]
 
 oscillator2Detune : Html Msg
 oscillator2Detune =
-    div
-        []
-        [ span 
-            [] 
-            [ "Oscillator 2 Detune" |> text ]
-        , input 
-            [ Html.Attributes.type' "range" 
-            , Html.Attributes.min "0"
-            , Html.Attributes.max "100"
-            , Html.Attributes.value "0"
-            , Html.Attributes.step "1"
-            , Html.Events.onInput Oscillator2DetuneChange
-            ]
-           []
-
-        ]
+  div
+      []
+      [ span 
+          [] 
+          [ "Oscillator 2 Detune" |> text ]
+      , input 
+          [ Html.Attributes.type' "range" 
+          , Html.Attributes.min "0"
+          , Html.Attributes.max "100"
+          , Html.Attributes.value "0"
+          , Html.Attributes.step "1"
+          , Html.Events.onInput Oscillator2DetuneChange
+          ]
+         []
+       ]
 
 oscillatorsBalance : Html Msg
 oscillatorsBalance =
-    div
-        []
-        [ span 
-            [] 
-            [ "Oscillators Balance" |> text ]
-        , input 
-            [ Html.Attributes.type' "range" 
-            , Html.Attributes.min "0"
-            , Html.Attributes.max "100"
-            , Html.Attributes.value "50"
-            , Html.Attributes.step "1"
-            , Html.Events.onInput OscillatorsBalanceChange
-            ]
-           []
-
-        ]
-
-        
+  div
+      []
+      [ span 
+          [] 
+          [ "Oscillators Balance" |> text ]
+      , input 
+          [ Html.Attributes.type' "range" 
+          , Html.Attributes.min "0"
+          , Html.Attributes.max "100"
+          , Html.Attributes.value "50"
+          , Html.Attributes.step "1"
+          , Html.Events.onInput OscillatorsBalanceChange
+          ]
+         []
+      ]
