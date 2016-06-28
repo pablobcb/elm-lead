00f2b95f389c3626e4c2dda4437828dc5531bcb3
Pablo Botelho <pablo.bcb@gmail.com>
2016-06-28 07:11:09 -0300

    decent log function implemented

--- src/js/AudioEngine/SynthEngine.js
+++ src/js/AudioEngine/SynthEngine.js
@@ -278,7 +278,8 @@ export default class SynthEngine {
 	}
 
 
-	setFilterCutoff = (freq) => {
+	setFilterCutoff = (midiValue) => {
+		const freq = 1.6 * 440 * Math.pow(2, (midiValue - 69) / 12)
 		this.filter.frequency.value = freq
 	}
 
