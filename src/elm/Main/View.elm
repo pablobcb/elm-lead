182ff039660edf8571d0d78c75d4eaa8ee55c171
Pablo Botelho <pablo.bcb@gmail.com>
2016-07-04 18:45:18 -0300

    added buttons

--- src/elm/Main/View.elm
+++ src/elm/Main/View.elm
@@ -2,8 +2,10 @@ module Main.View exposing (..)
 
 import Main.Model as Model exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Html exposing (..)
 import Container.OnScreenKeyboard.View as KbdView exposing (..)
+import Container.OnScreenKeyboard.Update as KbdUpdate exposing (..)
 import Container.Panel.View as PanelView exposing (..)
 import Main.Update as Update exposing (..)
 
@@ -13,32 +15,41 @@ view model =
     div [ class "dashboard" ]
         [ PanelView.panel PanelMsg
             model.panel
-        --, div [ class "panel__brand" ]
-            --[ span [] [ text "web lead" ]
-            --, span [] [ text "2X" ]
-            --, span [] [ text "virtual analog" ]
-            --]
+          --, div [ class "panel__brand" ]
+          --[ span [] [ text "web lead" ]
+          --, span [] [ text "2X" ]
+          --, span [] [ text "virtual analog" ]
+          --]
         , KbdView.keyboard OnScreenKeyboardMsg
             model.onScreenKeyboard
         , informationBar model
         ]
 
 
+incrementer : String -> String -> a -> a -> Html a
+incrementer label value up down =
+    div [ class "incrementer" ]
+        [ div [ class "incrementer__label" ] [ text label ]
+        , button
+            [ class "incrementer__btn"
+            , onClick <| down
+            ]
+            []
+        , span [ class "incrementer__label" ] [ text "-" ]
+        , div [ class "incrementer__display" ] [ text ("." ++value) ]
+        , span [ class "incrementer__label" ]
+            [ text "+" ]
+        , button
+            [ class "incrementer__btn"
+            , onClick <| up
+            ]
+            []
+        ]
+
+
 informationBar : Model -> Html Update.Msg
 informationBar model =
     let
-        startOctave =
-            toString model.onScreenKeyboard.octave
-
-        endOctave =
-            model.onScreenKeyboard.octave + 1 |> toString
-
-        octaveText =
-            "Octave is C" ++ startOctave ++ " to C" ++ endOctave
-
-        velocityText =
-            "velocity " ++ (toString model.onScreenKeyboard.velocity)
-
         midiIndicatorClass =
             "midi-indicator__status midi-indicator__status--"
                 ++ (if not model.midiSupport then
@@ -60,14 +71,21 @@ informationBar model =
                    )
     in
         div [ class "information-bar" ]
-            [ span [ class "information-bar__item" ]
-                [ octaveText |> text ]
-            , span [ class "information-bar__item" ] [ text velocityText ]
+            [ incrementer "Octave"
+                (" C" ++ (toString model.onScreenKeyboard.octave))
+                (OnScreenKeyboardMsg KbdUpdate.OctaveUp)
+                (OnScreenKeyboardMsg KbdUpdate.OctaveDown)
             , div [ class "midi-indicator" ]
                 [ div [ class blinkerClass ] []
                 , div [ class midiIndicatorClass ] []
                 ]
-            , a [ class "information-bar__gh-link"
+            , a
+                [ class "information-bar__gh-link"
                 , href "https://github.com/pablobcb/elm-lead"
-                ] []
+                ]
+                []
+            , incrementer "Velocity"
+                ((toString model.onScreenKeyboard.velocity))
+                (OnScreenKeyboardMsg KbdUpdate.VelocityUp)
+                (OnScreenKeyboardMsg KbdUpdate.VelocityDown)
             ]
