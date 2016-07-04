f8373f50420f2f8fd634245feec43ffd323ad7aa
Pablo Botelho <pablo.bcb@gmail.com>
2016-07-04 13:19:25 -0300

    wip

--- src/elm/Main/View.elm
+++ src/elm/Main/View.elm
@@ -13,11 +13,11 @@ view model =
     div [ class "dashboard" ]
         [ PanelView.panel PanelMsg
             model.panel
-        --, div [ class "dashboard__brand" ]
-        --    [ span [] [ text "web lead" ]
-        --    , span [] [ text "2X" ]
-        --    , span [] [ text "virtual analog" ]
-        --    ]
+        --, div [ class "panel__brand" ]
+            --[ span [] [ text "web lead" ]
+            --, span [] [ text "2X" ]
+            --, span [] [ text "virtual analog" ]
+            --]
         , KbdView.keyboard OnScreenKeyboardMsg
             model.onScreenKeyboard
         , informationBar model
