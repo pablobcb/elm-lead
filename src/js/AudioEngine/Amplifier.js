75b1489518f4d414661f89aa0ab691e5772cf20e
Pablo Botelho <pablo.bcb@gmail.com>
2016-07-06 13:18:55 -0300

    refined Amplifier.js class

--- src/js/AudioEngine/Amplifier.js
+++ src/js/AudioEngine/Amplifier.js
@@ -2,24 +2,27 @@ import ADSR from './ADSR'
 import MIDI from '../MIDI'
 
 export default class Amplifier {
-	constructor (context, state) {
-		this.state = state
+	constructor(context, state) {
 		this.context = context
 
-		this.adsr = new ADSR(this.context, state)
+		/* amp state */
+		this.state = {}
+		this.state.masterVolume = state.masterVolume
 
+		/* adsr state */
+		const { attack, decay, sustain, release} = state
+		this.adsr = new ADSR(this.context,
+			{ attack, decay, sustain, release })
+
+		/* AudioNode graph routing */
 		this.output = this.context.createGain()
 		this.output.gain.value = this.state.masterVolume
 		this.output.connect(this.context.destination)
 	}
-
-	_ = () => {}
 
 	setMasterVolumeGain = midiValue => {
 		const vol = MIDI.logScaleToMax(midiValue, 1)
 		this.state.masterVolume = vol
 		this.output.gain.value = vol
 	}
-
-	
 }
