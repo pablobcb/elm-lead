9b5fd8d5a157be2e30c59163aaa11a6b403bb166
Pablo Botelho <pablo.bcb@gmail.com>
2016-07-16 17:32:10 -0300

    added Osc1 Tests

--- test/AudioEngine/Osc1.spec.ts
+++ test/AudioEngine/Osc1.spec.ts
@@ -1,12 +1,10 @@
 import { expect } from 'chai'
 import { Osc1, Osc1State } from '../../src/ts/AudioEngine/Osc1'
 import CONSTANTS from '../../src/ts/Constants'
+import "web-audio-test-api"
 
 describe('Osc1', () => {
-	const context: AudioContext = {
-		createGain: () => ({})
-	} as AudioContext
-
+	const context:AudioContext = new AudioContext()
 	let osc1: Osc1
 
 	beforeEach(() => {
@@ -32,6 +30,13 @@ describe('Osc1', () => {
 		expect(osc1.outputs).to.have.lengthOf(CONSTANTS.MAX_VOICES)
 		for (let i = 0; i < CONSTANTS.MAX_VOICES; i++) {
 			expect(osc1.outputs[i]).to.exist
+		}
+	})
+
+	it('constructor should set 128 null voice oscs', () => {
+		expect(osc1.outputs).to.have.lengthOf(CONSTANTS.MAX_VOICES)
+		for (let i = 0; i < CONSTANTS.MAX_VOICES; i++) {
+			expect(osc1.vcos[i]).to.be.null
 		}
 	})
 
